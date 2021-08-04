76b7a76e6a88f647d2f3c37e47578f9d18fa8c3e
Natraj Bontha <nbontha@tradestation.com>
2021-08-04 13:27:52 -0500

    channels

---
+++ goconcurencybook/channel3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Like a river, a channel serves as a conduit for a stream of information;
+// values may be passed along the channel, and then read out downstream.
+// For this reason I usually end my chan variable names with the word “Stream.”
+// When using channels, you’ll pass a value into a chan variable, and then
+// somewhere else in your program read it off the channel. The disparate
+// parts of your program don’t require knowledge of each other, only a
+// reference to the same place in memory where the channel resides.
+// This can be done by passing references of channels around your program.
+
+// Read and Write Channel example
+// var dataStream chan interface{}
+// 	dataStream = make(chan interface{})
+
+// Read or Receive only Channel example
+// var dataStream <- chan interface{}
+// dataStream := make( <-chan interface{})
+
+// Send or Write only Channel example
+// var dataStream chan <- interface{}
+// 	dataStream := make(chan <- interface{})
+
+// Unblocking multiple go routines at once
+func main() {
+	begin := make(chan interface{})
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			<-begin // Here the goroutine waits until it is told it can continue.
+			fmt.Printf("%v has begun\n", i)
+		}(i)
+	}
+	fmt.Println("Unblocking goroutines...")
+	close(begin) //Here we close the channel, thus unblocking all the goroutines simultaneously.
+	wg.Wait()
+
+}
